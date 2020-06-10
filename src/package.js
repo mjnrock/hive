64331b580f94ab4abf08f792e2dc560bed2265a2
Matt Nawrocki <mjnrock@umich.edu>
2020-06-10 19:00:10 -0400

    init

--- src/package.js
+++ src/package.js
@@ -1,11 +1,9 @@
 import fn from "./functions";
 import Node from "./Node";
 import Message from "./Message";
-import { spawnStateNode } from "./state";
-import { useNodeContext } from "./hooks";
 
-export const spawnStateNode;
-export const useNodeContext;
+export { spawnStateNode } from "./state";
+export { useNodeContext } from "./hooks";
 
 export default {
     Node,
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
-import Hive from "./../lib/package";
+import { spawnStateNode } from "./../lib/package";
 
-console.log(Hive.spawnStateNode({ cat: 5 }, ["cats", (state, msg) => {
+console.log(spawnStateNode({ cat: 5 }, ["cats", (state, msg) => {
     return {
         ...state,
         cats: msg.payload
